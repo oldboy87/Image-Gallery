66e5109701493ef538f0bc1b17fe45ac8962351b
oldboy87 <tomstevenson87@gmail.com>
2025-11-30 10:28:55 +0000

    chore: making interface more responsive

--- app.js
+++ app.js
@@ -72,20 +72,6 @@ function wantSize(reqSize) {
 //TODO: Do NOT forget this is how to properly reference objects within objects:
 // console.log(imgData[0].imgSRC.small);
 
-//TODO: STEP2: create thumbnail images
-//This is the rough outline we started with:
-// function createThumbnails(){
-//     select the DOM element (thumbnail-container) to contain the thumbnails
-//     this is a repetitive task --> for loop (for, forEach())...)
-//     Loop task:
-//     -create img element
-//     - update src and alt attributes (properties) of the img element to match those in the array (parameters)
-//     - give each img a className (img.className)
-//     - add an event listener to each image --> the event handler of this event is the function you write to create the fullscreen images
-//     - append the created images to the thumbnail-container
-
-// }
-
 function createThumbnails() {
   // select the DOM element (thumbnail-container) to contain the thumbnails
   const container = document.getElementById("thumbnail-container");
@@ -101,6 +87,8 @@ function createThumbnails() {
     // - update src and alt attributes (properties) of the img element to match those in the array (parameters)
     thumbnail.setAttribute("src", attSrc);
     thumbnail.setAttribute("alt", attAlt);
+    //Sets "thumbnail" ID to images
+    thumbnail.setAttribute("id", "thumbnail");
     //TODO: Come back to WIDTH and HEIGHT
     // thumbnail.setAttribute("width", "300");
     // - give each img a className (img.className)
@@ -118,64 +106,10 @@ function createThumbnails() {
   }
 }
 
-//TODO THIS MIGHT ACTUALLY HELP
-//TODO THIS MIGHT ACTUALLY HELP
-//TODO THIS MIGHT ACTUALLY HELP
-//TODO THIS MIGHT ACTUALLY HELP
-//TODO THIS MIGHT ACTUALLY HELP
-
-// const usethis = imgData.find(function (item) {
-//   return item.imgClass === "nameTest01";
-// });
-
-// console.log(usethis);
-
-// function imgFinder(className) {
-//   return className;
-// }
-
-// const leg = imgData.find(imgFinder("nameTest01"));
-
-// console.log(shit.findIndex(imgFinder("4")));
-
-// console.log(imgFinder("fuck off"));
-
-// function leg{
-// imgData.find(imgFinder(clickedClass))
-// }
-
-//TODO /END OF EXAMPLE find()
-
 //STEP3: create fullscreen images
 function createFullscreenImages(thumbnail) {
   //this is the event handler
   //logs the HTML element object reference and also the derived class name, which is stored as a variable
-  // console.log(thumbnail);
-  // //TODO BACKUP START
-  // const clickedImg = thumbnail.originalTarget;
-  // const clickedSrc = clickedImg.src;
-  // const clickedAlt = clickedImg.alt;
-  // const clickedClass = clickedImg.className;
-
-  // console.log(clickedImg);
-  // console.log(clickedSrc);
-  // console.log(clickedAlt);
-  // console.log(clickedClass);
-
-  // const getImgData = imgData.find(function (item) {
-  //   return item.imgClass === clickedClass;
-  // });
-
-  // //TODO do NOT mess with the above!
-
-  // const getImgSrc = getImgData.imgSrc;
-  // const getImgAlt = getImgData.imgAlt;
-  // const getImgPicked = getImgSrc[pickedSize];
-
-  // console.log(getImgData);
-  // console.log(getImgSrc);
-  // console.log(getImgAlt);
-  // console.log(getImgPicked);
 
   // //TODO BACKUP END
   //TODO if I want to use IDs for each image size, I can do so here as well
